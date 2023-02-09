1665db91a845e8c185f1b335e1a8b7194051fd5c
jagrutipatil3030 <102649317+jagrutipatil3030@users.noreply.github.com>
2023-02-09 15:16:19 +0530

    Update simple.cs

--- simple.cs
+++ simple.cs
@@ -1,15 +1,16 @@
 using System;
-using System.Collection.Generic;
-using Systen.Linq;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
-namespace simplemessag
+namespace simplemessage
 {
- class program
- {
-   static void main(String args[])
-   {
-     Console.WriteLine(" Each One,Tech One,Tree One ");
-     }
-  }
-}  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Each One Teach One Tree One");
+            Console.ReadLine();
+        }
+    }
+} 
