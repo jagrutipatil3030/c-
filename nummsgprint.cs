22bb56f1382adc33b79800ac0a88864d77710e82
jagrutipatil3030 <102649317+jagrutipatil3030@users.noreply.github.com>
2023-02-09 15:10:30 +0530

    Update and rename notimemessg.program to nummsgprint.cs

---
+++ nummsgprint.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace print
+{
+    class demo 
+    {
+         int i,num;
+        public void get()
+        {
+            Console.WriteLine("Enter the repetiton of line ");
+            num= Convert.ToInt16(Console.ReadLine());
+        }
+       public void show()
+        {
+            for (i = 1; i <= num; i++)
+            {
+                Console.WriteLine("Hellow ");
+            }
+            Console.ReadLine();
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            demo d = new demo();
+            d.get();
+            d.show();
+        }
+    }
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
